a631c63b1d97bc7e9c759c3131000b5ab8978b79
Luisa Martinez <isalou04@gmail.com>
2026-01-08 17:34:15 -0600

    very basic game setup-- admirer and 2 npcs, no animation, simple/stand-in tile map; admirer + npc has template scripts; note: I forgot to add wall boundries, so disregard the admirer walking through walls, will fix.

---
+++ scripts/admirer.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+# NOTE: BECAUSE THE GAME IS TOP-DOWN (no jumping?) WE DON'T NEED 'GRAVITY'
+# the character just needs to move up/down/sides
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
