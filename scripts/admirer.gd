f361fccc3c137bdb89fb6aa978b98bb9fab2b8fb
Luisa Martinez <isalou04@gmail.com>
2026-01-15 16:03:54 -0600

    wasd changes

--- scripts/admirer.gd
+++ scripts/admirer.gd
@@ -1,26 +1,11 @@
 extends CharacterBody2D
-# NOTE: BECAUSE THE GAME IS TOP-DOWN (no jumping?) WE DON'T NEED 'GRAVITY'
-# the character just needs to move up/down/sides
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	get_input()
 	move_and_slide()
